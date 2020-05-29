a0d8de7fe55942221171fe45dfcc5c2903ec5ef3
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-05-29 13:23:49 -1000

    added scrolling background

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+
+    [SerializeField] float backgroundSrollSpeed = 0.5f;
+    Material myMaterial;
+    Vector2 offSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+        offSet = new Vector2(0f, backgroundSrollSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
